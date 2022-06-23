23d84d5c5b7130417db824d1f38d4c69faa3ae24
tomelovich <tomelovichs@gmail.com>
2022-06-23 16:06:22 +0300

    Выполнено

---
+++ 34/AppSample/AppSample/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace AppSample
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+            => Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+    }
+}
